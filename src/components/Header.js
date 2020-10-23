68bbcadef735a43a42cecde9e3a5dc3b355e00f2
Luis <luis@mbp.mynet>
2020-10-23 11:04:46 +0100

    add cv

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,8 +15,9 @@ const Header = () => {
   return (
     <nav className="flex items-center justify-between flex-wrap bg-gray-900 px-4 py-3 fixed w-full z-10 top-0 shadow-md" style={{ height: 60 }}>
       <div className="flex items-center flex-shrink-0 text-white ml-2">
-        <a href="https://barcel0.github.io/me" className="no-underline hover:no-underline tracking-wider text-xl font-semibold" >
-          <span><IconFingerprint size={6} /></span>
+        <a href="https://barcel0.github.io/me" className="flex no-underline hover:no-underline tracking-wider text-xl font-semibold" >
+          <span className="mr-2"><IconFingerprint size={6} /></span>
+          <h1>luis barceló</h1>
         </a>
       </div>
       <div className="block lg:hidden">
@@ -29,23 +30,25 @@ const Header = () => {
           <li className="flex items-center mr-3">
             <a href='#about' className="link inline-block py-0 px-4">01. About</a>
           </li>
-          <li className="flex items-center mr-3 hover:text-underline">
+          <li className="flex items-center mr-3">
             <a href='#stack' className="link inline-block py-0 px-4">02. Technologies</a>
           </li>
-          <li className="flex items-center mr-3 hover:text-underline">
+          <li className="flex items-center mr-3">
             <a href='#portfolio' className="link inline-block py-0 px-4">03. Portfolio</a>
           </li>
-          <li className="flex items-center mr-3 hover:text-underline">
+          <li className="flex items-center mr-3">
             <a href='#education' className="link inline-block py-0 px-4">04. Education</a>
           </li>
-          <li className="flex items-center mr-3 hover:text-underline">
+          <li className="flex items-center mr-3">
             <a href='#contact' className="link inline-block py-0 px-4">05. Contact</a>
+          </li>
+          <li className="flex items-center mr-3 font-bold border-2 border-orange-500 rounded-md ">
+            <a href='./pdf/cv-2021-lbarcelo.pdf' download className="link inline-block py-0 px-4">CV</a>
           </li>
         </ul>
       </div>
     </nav >
   );
 }
-
 
 export default (Header);
