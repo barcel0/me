9546635078fc281f9eb0d7f19825ad778f426781
luis <lbarcelocarrera@gmail.com>
2020-11-06 17:16:07 +0000

    about update

--- src/components/About.js
+++ src/components/About.js
@@ -14,10 +14,10 @@ const About = () => {
             Hi there, my name is <span className="text-orange-400 font-bold">Luis Barceló</span> and I'm a <span className="text-orange-400 font-bold">Junior JavaScript Front End Developer</span>.
           </p>
           <p>
-            I enjoy creating new applications as well as learning new technologies and methodologies that can help you producing <span className="text-orange-400 font-bold">neat code</span>. During my learning path, I have been focused on <span className="text-orange-400 font-bold">React JS</span> as well as <span className="text-orange-400 font-bold">React Native</span>, while trying to build at the same time a solid javascript foundation.
+            I enjoy creating new applications as well as learning new technologies and methodologies that can help you producing <span className="text-orange-400 font-bold">neat code</span>. During my learning, I have been focused on <span className="text-orange-400 font-bold">React JS</span> as well as <span className="text-orange-400 font-bold">React Native</span>, while trying to build at the same time a solid JavaScript foundation.
             </p>
           <p>
-            Always have had a genuine interest in <span className="text-orange-400 font-bold">software development</span> from a young age, even during the last years, while working as an architect. I also have interest in economics, especially in the futures market, where I like to trade every now and then.
+            Always have had a genuine interest in <span className="text-orange-400 font-bold">software development</span> from a young age, even during the last years, while working as an Architect. I also have interest in finance, especially in the futures market, where I like to trade every now and then.
           </p>
         </div>
         <div className="w-2/5 flex justify-center">
